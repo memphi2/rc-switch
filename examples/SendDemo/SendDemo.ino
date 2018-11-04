b03d2adc4e63cb23e2fd5cc667ae1c96971e539a
memphi2 <memphiz@gmx.net>
2018-11-04 20:52:19 +0100

    Add files via upload

--- examples/SendDemo/SendDemo.ino
+++ examples/SendDemo/SendDemo.ino
@@ -1,9 +1,12 @@
 /*
   Example for different sending methods
   
-  https://github.com/sui77/rc-switch/
-  
-*/
+ /*
+ Usage: sendMC(<command>, <dataLength>, <syncLength>, <repeat of command>, <delay between commands>)
+ commands are 0,1 and 2,3 for Sync
+ *mySwitch.sendMC("301110100010111300001000130000100013",1780,5000,3,10000);
+ */
+
 
 #include <RCSwitch.h>
 
@@ -15,12 +18,12 @@ void setup() {
   
   // Transmitter is connected to Arduino Pin #10  
   mySwitch.enableTransmit(10);
-  
-  // Optional set protocol (default is 1, will work for most outlets)
-  // mySwitch.setProtocol(2);
 
   // Optional set pulse length.
   // mySwitch.setPulseLength(320);
+  
+  // Optional set protocol (default is 1, will work for most outlets)
+  // mySwitch.setProtocol(2);
   
   // Optional set number of transmission repetitions.
   // mySwitch.setRepeatTransmit(15);
@@ -30,28 +33,9 @@ void setup() {
 void loop() {
 
   /* See Example: TypeA_WithDIPSwitches */
-  mySwitch.switchOn("11111", "00010");
+  mySwitch.sendMC("301110100110001300001000130000100013",1780,5000,3,10000); //shutter up
   delay(1000);
-  mySwitch.switchOff("11111", "00010");
-  delay(1000);
+  mySwitch.sendMC("300110100110001300001000130000100013",1780,5000,3,10000); //shutter stop
+  delay(60000);
 
-  /* Same switch as above, but using decimal code */
-  mySwitch.send(5393, 24);
-  delay(1000);  
-  mySwitch.send(5396, 24);
-  delay(1000);  
-
-  /* Same switch as above, but using binary code */
-  mySwitch.send("000000000001010100010001");
-  delay(1000);  
-  mySwitch.send("000000000001010100010100");
-  delay(1000);
-
-  /* Same switch as above, but tri-state code */ 
-  mySwitch.sendTriState("00000FFF0F0F");
-  delay(1000);  
-  mySwitch.sendTriState("00000FFF0FF0");
-  delay(1000);
-
-  delay(20000);
 }
